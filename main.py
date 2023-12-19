99c02c8a5e948fdb89a3cb2ec0d21304bc66fc62
oldmc <oldmcdonnell@gmail.com>
2023-12-18 20:55:44 -0500

    refined variables created static folder

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
+import pandas
 
-app = Flask("Website")
+app = Flask(__name__)
+
 
 @app.route("/home/")
 def home():
-    return render_template("tutorial.html")\
+    return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
 
-app.run(debug=True)
+#get reading for different stations variable
+@app.route("/api/v1/<station>/<date>")
+def about(dtation, date):
+    df = pandas.read_csv("")
+    temperature = df.station(date)
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+#only run flask script from the main page
+if __name__ == "__main__":
+    app.run(debug=True)
